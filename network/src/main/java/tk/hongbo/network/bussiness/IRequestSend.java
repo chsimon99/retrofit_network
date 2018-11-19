6f993cae22e6740ed8b44dbb1a79e0d10599af74
HONGBO <zhzephizhao@huangbaoche.com>
2018-11-19 21:06:43 +0800

    接口日志汇报

--- network/src/main/java/tk/hongbo/network/bussiness/IRequestSend.java
+++ network/src/main/java/tk/hongbo/network/bussiness/IRequestSend.java
@@ -4,9 +4,10 @@ import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
+import tk.hongbo.network.data.NetRoot;
 
 public interface IRequestSend {
 
     @POST("passport/v1.0/report")
-    Call<String> sendReporrt(@Body RequestBody jsonArray);
+    Call<NetRoot> sendReporrt(@Body RequestBody jsonArray);
 }
